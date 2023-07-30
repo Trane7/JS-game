37ef256b7b9b236ea00f60f68ac37ed2803d4f45
Dee <dgrowette@gmail.com>
2023-07-30 12:01:52 -0700

    added div tag to peices

--- pieces.js
+++ pieces.js
@@ -1,6 +1,6 @@
-const king = <font-awesome-icon icon="chess-king" />
-const queen = <font-awesome-icon icon="chess-queen" />
-const knight = <font-awesome-icon icon="chess-knight" />
-const rook =  <font-awesome-icon icon="chess-rook" />
-const bishop = <font-awesome-icon icon="chess-bishop" />
-const pawn = <font-awesome-icon icon="chess-pawn" />
+const king = <div class="piece" id="king"><font-awesome-icon icon="chess-king" /></div>
+const queen = <div class="piece" id="queen"><font-awesome-icon icon="chess-queen" /></div>
+const knight = <div class="piece" id="knight"><font-awesome-icon icon="chess-knight" /></div>
+const rook =  <div class="piece" id="rook"><font-awesome-icon icon="chess-rook" /></div>
+const bishop = <div class="piece" id="bishop"><font-awesome-icon icon="chess-bishop" /></div>
+const pawn = <div class="piece" id="pawn"><font-awesome-icon icon="chess-pawn" /></div>
