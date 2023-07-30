fe0d1c8e832df0f5f7e7daf6251c261e317f4867
Dee <dgrowette@gmail.com>
2023-07-30 11:50:21 -0700

    added chess peices

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const gameBoard = document.querySelector("#board")
 const playerDisplay = document.querySelector('#player')
 const info = document.querySelector('#info')
+const width = 8
+
+const startPieces = [
+    
+]  
---
+++ pieces.js
@@ -0,0 +1,6 @@
+const king = <font-awesome-icon icon="chess-king" />
+const queen = <font-awesome-icon icon="chess-queen" />
+const knight = <font-awesome-icon icon="chess-knight" />
+const rook =  <font-awesome-icon icon="chess-rook" />
+const bishop = <font-awesome-icon icon="chess-bishop" />
+const pawn = <font-awesome-icon icon="chess-pawn" />
