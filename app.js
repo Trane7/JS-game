05c22a806a14743747456a45fd99a0e4ef6dc52b
Dee <dgrowette@gmail.com>
2023-07-31 21:37:34 -0700

    added to stylesheet and function to app.js

--- app.js
+++ app.js
@@ -4,5 +4,20 @@ const info = document.querySelector('#info')
 const width = 8
 
 const startPieces = [
-    
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook
 ]  
+
+function createBoard () {
+    startPieces.forEach((startPiece) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        gameBoard.append(square)
+    })
+}
